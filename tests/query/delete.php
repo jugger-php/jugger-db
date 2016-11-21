c715ddea809c319161836fa9c323575dbc83efb9
irpsv <i.rpsv@live.com>
2016-11-21 22:32:28 +0500

    create caps (заглушки)

---
+++ tests/query/delete.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use jugger\db\Query;
+
+class Test extends TestCase
+{
+
+    /**
+     *
+     * @dataProvider dataProvider
+     */
+    public function test($table, $on, $sql)
+    {
+        
+    }
+
+    public function dataProvider()
+    {
+        return [
+
+        ];
+    }
+}
---
+++ tests/query/limit.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use jugger\db\Query;
+
+class LimitTest extends TestCase
+{
+
+    /**
+     *
+     * @dataProvider dataProvider
+     */
+    public function test($table, $on, $sql)
+    {
+
+    }
+
+    public function dataProvider()
+    {
+        return [
+
+        ];
+    }
+}
