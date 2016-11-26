8de159de1bc74f620d2e2acf518675a8aca8a5e9
irpsv <i.rpsv@live.com>
2016-11-26 18:13:34 +0500

    add tests

--- tests/query/having.php
+++ tests/query/having.php
@@ -1,24 +1,28 @@
 <?php
-// 
-// use PHPUnit\Framework\TestCase;
-// use jugger\db\Query;
-//
-// class HavingTest extends TestCase
-// {
-//
-//     /**
-//      *
-//      * @dataProvider dataProvider
-//      */
-//     public function testJoin($table, $on, $sql)
-//     {
-//
-//     }
-//
-//     public function dataProvider()
-//     {
-//         return [
-//
-//         ];
-//     }
-// }
+
+use PHPUnit\Framework\TestCase;
+use jugger\db\Query;
+
+class HavingTest extends TestCase
+{
+
+    /**
+     *
+     * @dataProvider dataProvider
+     */
+    public function test($sql, $params)
+    {
+        $q = (new Query())->from('t1')->having($params);
+        $this->assertEquals($sql, $q->build());
+    }
+
+    public function dataProvider()
+    {
+        return [
+            [
+                "SELECT * FROM t1 HAVING COUNT(*) > 123",
+                "COUNT(*) > 123"
+            ],
+        ];
+    }
+}
