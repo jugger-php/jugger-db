fc9e18b1734108d0540e30827a0914b139f241ea
irpsv <i.rpsv@live.com>
2016-11-26 18:13:25 +0500

    add tests

--- tests/query/groupby.php
+++ tests/query/groupby.php
@@ -10,15 +10,23 @@ class GroupByTest extends TestCase
      *
      * @dataProvider dataProvider
      */
-    public function testJoin($table, $on, $sql)
+    public function test($sql, $params)
     {
-
+        $q = (new Query())->from('t1')->groupBy($params);
+        $this->assertEquals($sql, $q->build());
     }
 
     public function dataProvider()
     {
         return [
-            
+            [
+                "SELECT * FROM t1 GROUP BY col1, col2, col3",
+                "col1, col2, col3"
+            ],
+            [
+                "SELECT * FROM t1 GROUP BY `col1`, `col2`, `col3`",
+                ["col1", "col2", "col3"]
+            ],
         ];
     }
 }
