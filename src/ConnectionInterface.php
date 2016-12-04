8721af7809b3700cff9a1e7583cb53163dbfc778
irpsv <i.rpsv@live.com>
2016-12-04 22:23:46 +0500

    add transaction methods

--- src/ConnectionInterface.php
+++ src/ConnectionInterface.php
@@ -15,17 +15,25 @@ interface ConnectionInterface
      * @param  string  $sql запрос
      * @return integer      количество измененых (добавленыъ) строк
      */
-    public function execute(string $sql);
+    public function execute(string $sql): int;
     /**
      * Подготавливает значение
      * @param  mixed $value значение, которое необходимо подготовить
      * @return string       значение, защищенное от SQL инъекции
      */
-    public function escape($value);
+    public function escape($value): string;
     /**
      * Оборачивает значение в кавычки
      * @param  string    $value имя столбца, таблицы, базы
      * @return string           значение обернутое в кавычки
      */
-    public function quote(string $value);
+    public function quote(string $value): string;
+
+    public function beginTransaction();
+
+    public function commit();
+
+    public function rollBack();
+
+    public function getLastInsertId($tableName = null): int;
 }
