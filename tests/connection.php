4836be277eeda4f1bd8df9a38ae7686cb31c46a4
irpsv <i.rpsv@live.com>
2017-01-22 18:04:21 +0500

    add many connections test

--- tests/connection.php
+++ tests/connection.php
@@ -7,40 +7,65 @@ use jugger\db\QueryResult;
 
 class ConnectionTest extends TestCase
 {
-    public static function setUpBeforeClass()
+    public function dataProvider()
     {
-        $sql = "CREATE TABLE `t2` (`id` INTEGER, `name` TEXT)";
-        Di::$pool['default']->execute($sql);
-    }
+        $sqlite = Di::$pool['default'];
+        $sqlite->execute("DROP TABLE IF EXISTS `t2`");
+        $sqlite->execute("CREATE TABLE IF NOT EXISTS `t2` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `name` TEXT)");
 
-    public static function tearDownAfterClass()
-    {
-        Di::$pool['default']->execute("DROP TABLE `t2`");
-    }
+        $mysql = Di::$pool['mysql'];
+        $mysql->execute("DROP TABLE IF EXISTS `t2`");
+        $mysql->execute("CREATE  TABLE IF NOT EXISTS `t2` (`id` INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, `name` TEXT)");
 
-    public function testExecuteDb()
-    {
-        $db = Di::$pool['default'];
-        $sql = "INSERT INTO `t2` VALUES(1, 'value')";
-        $this->assertTrue($db->execute($sql) == 1);
+        return [
+            [$sqlite],
+            [$mysql],
+        ];
     }
 
     /**
+     * @dataProvider dataProvider
+     */
+    public function testExecuteDb($db)
+    {
+        $sql = "INSERT INTO `t2` VALUES(1, 'value')";
+        $this->assertTrue($db->execute($sql) == 1);
+
+        $db->execute("INSERT INTO `t2`(name) VALUES('value2')");
+        $db->execute("INSERT INTO `t2`(name) VALUES('value3')");
+        $db->execute("INSERT INTO `t2`(name) VALUES('value4')");
+        $db->execute("INSERT INTO `t2`(name) VALUES('value5')");
+
+        $sql = "DELETE FROM t2 WHERE id > 3";
+        $rows = $db->execute($sql);
+        $this->assertTrue($rows == 2);
+    }
+
+    /**
+     * @dataProvider dataProvider
      * @depends testExecuteDb
      */
-    public function testQuery()
+    public function testQuery($db)
     {
-        $result = Di::$pool['default']->query("SELECT id, name FROM t2");
+        $result = $db->query("SELECT id, name FROM t2");
         $this->assertInstanceOf(QueryResult::class, $result);
 
         $row = $result->fetch();
         $this->assertEquals($row['id'], 1);
         $this->assertEquals($row['name'], 'value');
+
+        $result = $db->query("SELECT id, name FROM t2");
+        $rows = $result->fetchAll();
+        $this->assertEquals($result->count(), 3);
+        $this->assertEquals(count($rows), 3);
     }
 
-    public function testQuote()
+    /**
+     * @dataProvider dataProvider
+     * @depends testQuery
+     */
+    public function testQuote($db)
     {
-        $db = Di::$pool['default'];
         $data = [
             "keyword" => "`keyword`",
             "table_name.column_name" => "`table_name`.`column_name`",
@@ -48,5 +73,60 @@ class ConnectionTest extends TestCase
         foreach ($data as $value => $etalon) {
             $this->assertEquals($etalon, $db->quote($value));
         }
+    }
+
+    /**
+     * @dataProvider dataProvider
+     * @depends testQuote
+     */
+    public function testEscape($db)
+    {
+        $this->assertEquals(
+            "LIKE '%\' AND id LIKE \'1%'",
+            "LIKE '%". $db->escape("' AND id LIKE '1") ."%'"
+        );
+    }
+
+    /**
+     * @dataProvider dataProvider
+     * @depends testEscape
+     */
+    public function testAcid($db)
+    {
+        // commit
+        $db->beginTransaction();
+        $db->execute("INSERT INTO t2(name) VALUES('value2')");
+
+        $rowId = $db->getLastInsertId();
+        $db->commit();
+
+        $this->assertEquals($rowId, 6);
+
+        $row = $db->query("SELECT id, name FROM t2 WHERE id = {$rowId}")->fetch();
+        $this->assertEquals($row['name'], 'value2');
+
+        // rollBack
+        $db->beginTransaction();
+        $db->execute("INSERT INTO t2(name) VALUES('value3')");
+
+        $rowId = $db->getLastInsertId();
+        $db->rollBack();
+
+        $this->assertEquals($rowId, 7);
+
+        $row = $db->query("SELECT id, name FROM t2 WHERE id = {$rowId}")->fetch();
+        $this->assertNull($row);
+
+        $row = $db->query("SELECT id, name FROM t2 ORDER BY id DESC")->fetch();
+        $this->assertEquals($row['id'], 6);
+    }
+
+    /**
+     * @dataProvider dataProvider
+     * @depends testAcid
+     */
+    public function testDrop($db)
+    {
+        $db->execute("DROP TABLE IF EXISTS `t2`");
     }
 }
